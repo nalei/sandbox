8b1042e5fcee41d3bdf9050fd20d7776e3b3d6b6
Yuri Anikin <yanikin@voximplant.com>
2019-02-20 16:26:16 +0300

    Build

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -2,19 +2,14 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.config.js')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const UglifyWebpackPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(baseConfig, {
   entry: {
     app: './src/entry-client.js'
   },
   optimization: {
-    splitChunks: {
-      minSize: 100000,
-      chunks: 'initial',
-      maxAsyncRequests: Infinity,
-      maxInitialRequests: Infinity,
-      name: true
-    }
+    minimizer: [new UglifyWebpackPlugin({ sourceMap: true })]
   },
   plugins: [
     new webpack.EnvironmentPlugin(['APP_LOCALE']),
